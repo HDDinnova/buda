ed1ca70cd4cbdb50acdb920c34d5664df4abdf54
HDDinnova Web Services <info@hddinnova.com>
2015-09-18 21:07:41 +0200

    20150918

--- Buda Interactiu/index.php
+++ Buda Interactiu/index.php
@@ -1,10 +1,60 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title></title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Buda, l'illa del Delta</title>
+        <meta name="description" content="Documental interactiu sobre Buda, la illa del Delta">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        <link href="css/estil.css" rel="stylesheet" type="text/css"/>
+        <link href="css/inici.css" rel="stylesheet" type="text/css"/>
     </head>
     <body>
-        
+        <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <!--<a class="navbar-brand" href="/">BUDA <span>L'illa del Delta</span></a>-->
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><img src="img/menu-menu.png" alt=""/></a></li>
+                
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div><!-- /.container-fluid -->
+        </nav>
+        <!--Fi capçalera-->
+        <img id="fletxa1" src="img/fletxa-1.png" alt="fletxa horitzontal entra per esquerra"/>
+        <img id="fletxa2" src="img/fletxa-2.png" alt="fletxa curvada gira per mostrar personatges"/>
+        <img id="pfoto" src="img/personatge-foto.png" alt="imatge perfil fotograf"/>
+        <img id="cfoto" src="img/personatge-foto-colors.png" alt="colors imatge perfil fotograf"/>
+        <img id="porni" src="img/personatge-orni.png" alt="imatge perfil ornitoleg"/>
+        <img id="corni" src="img/personatge-orni-colors.png" alt="colors imatge perfil ornitoleg"/>
+        <img id="plite" src="img/personatge-lite.png" alt="imatge perfil escriptor"/>
+        <img id="clite" src="img/personatge-lite-colors.png" alt="colors imatge perfil escriptor"/>
+        <img id="pcine" src="img/personatge-cine.png" alt="imatge perfil cineasta"/>
+        <img id="ccine" src="img/personatge-cine-colors.png" alt="colors imatge perfil cineasta"/>
+        <img id="fletxa3" src="img/fletxa-3.png" alt="fletxa inclinada per mostrar últim personatge"/>
+        <div class="container-fluid" id="titol">
+            <h1 id="t1">Buda</h1>
+            <h2 id="t2">L'illa del Delta</h2>
+            <p id="t3">Documental interactiu</p>
+            <div class="caixa enhorabona verd">
+                <p id="enhp1">Enhorabona!</p>
+                <p id="enhp2">Tens l'autorització per</p>
+                <p id="enhp3">entrar a l'illa de Buda</p>
+            </div>
+            <div class="caixa ambit taronga">
+                <p id="ambp1">Quin és el teu àmbit</p>
+                <p id="ambp2">de treball?</p>
+            </div>
+            <div class="caixa mini literatura groc">
+                <p id="lit">Literatura</p>
+            </div>
+        </div>
     </body>
 </html>
