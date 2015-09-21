91d08afe9d826abe645b0ac08a726d87c0ffccc8
EE <EE@EE-PC>
2015-09-21 14:21:14 +0200

    20150921

--- Buda Interactiu/js/variables.js
+++ Buda Interactiu/js/variables.js
@@ -1,4 +1,4 @@
-var c1='437px',c2='492px',c3='547px',c4='602px';
+var c1='397px',c2='452px',c3='507px',c4='562px';
 var tlite='Sempre t’ha fascinat el seu nom. Estàs buscant inspiració per ambientar el teu proper relat. Intueixes que a l’illa de Buda t’esperen vivències que mai oblidaràs.';
 var tcine='La silueta d’un far menjat per la mar, majestuós i inaprensible, et persegueix des de fa temps. Vols prendre notes visuals de cara al teu proper film.';
 var tfoto='El reclam d’un món verge et sedueix cada cop més. Voldries capturar amb la càmera les ombres del que queda d’aquell univers fràgil i poderós.';
