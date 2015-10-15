559c040c637838c6782edfadd0b5114cbc60848b
EE <EE@EE-PC>
2015-10-15 14:44:56 +0200

    20151015

--- Buda Interactiu/01pasbuda/js/funcions.js
+++ Buda Interactiu/01pasbuda/js/funcions.js
@@ -119,6 +119,16 @@ function controlMouse(e){
                         controlMouse(e);
                     });
                 },1000);
+                break;                
+            case 42:
+                $('body').off('mousewheel');
+                diapositiva50();
+                setTimeout(function(){
+                    pos=50;
+                    $('body').on('mousewheel',function(e){
+                        controlMouse(e);
+                    });
+                },1000);
                 break;
         }
     } else if(e.deltaY>0) {
@@ -731,5 +741,28 @@ function diapositiva42(){
     },trans,'swing');
     $('#imgbarcassa2 p').delay('1100').animate({
         'bottom':'10px'
+    },trans,'swing');
+    $('#apartat5').load('apartat5.html');
+}
+function diapositiva50(){
+    $('#apartat5').show();
+    $('#imgbarcassa2 p').animate({
+        'bottom':'-50px'
+    },trans,'swing');
+    $('#titol4-2').delay('100').animate({
+        'top':'-50%'
+    },trans,'swing');
+    $('#destacat4-2').delay('300').animate({
+        'top':'-50%'
+    },trans,'swing');
+    $('#play4').delay('600').animate({
+        'bottom':'150%'
+    },trans,'swing');
+    $('#apartat5 video').get(0).play();
+    $('#imgbarcassa2 img').delay('800').animate({
+        'top':'100%'
+    },trans,'swing');
+    $('#apartat5 video').delay('800').animate({
+        'bottom':'0%'
     },trans,'swing');
 }
